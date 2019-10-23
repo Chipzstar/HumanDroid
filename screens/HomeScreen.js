5f92bdbcc550c960808f9b41407267bf64db45c0
Chisom Oguibe <chisom.oguibe@spicymango.co.uk>
2019-10-23 13:05:20 +0100

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,32 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+        <Text></Text>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  }
+});
