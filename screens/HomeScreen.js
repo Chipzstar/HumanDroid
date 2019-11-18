5c1f9b98a3dbc673fcd7ddc52e208afea1964d28
yisi98 <53112658+yisi98@users.noreply.github.com>
2019-11-18 16:57:32 +0000

    background image
    testing the background image

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,32 +1,50 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    useState,
+    Component,
+    ImageBackground
 } from 'react-native';
 
+
+
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-        <Text></Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                source={require('../../ProjectX/assets/images/greenBackground.png')}
+                style={{width: '100%', height: '100%'}}>
+                <TouchableOpacity>
+                    <Image
+                        style={{width: 250, height: 250}}
+                        source={require('../../ProjectX/assets/images/UploadButton.png')}
+                    />
+                </TouchableOpacity>
+            </ImageBackground>
+        </View>
+    );
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+    header: null,
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  }
+    image: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    }
 });
